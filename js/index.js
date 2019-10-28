cbd5adf1daef9cb58fd948b7d1caf4dab51d60fb
Darragh Ferry <darraghferry@hotmail.com>
2019-10-28 13:20:53 +0000

    completed tasks 1 and 2

--- js/index.js
+++ js/index.js
@@ -40,3 +40,35 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+let aTags = document.querySelectorAll("a");
+aTags[0].textContent = siteContent["nav"]["nav-item-1"];
+aTags[1].textContent = siteContent["nav"]["nav-item-2"];
+aTags[2].textContent = siteContent["nav"]["nav-item-3"];
+aTags[3].textContent = siteContent["nav"]["nav-item-4"];
+aTags[4].textContent = siteContent["nav"]["nav-item-5"];
+aTags[5].textContent = siteContent["nav"]["nav-item-6"];
+let codePicture = document.getElementById("cta-img");
+codePicture.setAttribute('src', siteContent['cta']['img-src'])
+let heading = document.querySelector('h1');
+heading.textContent = siteContent['cta']['h1'];
+let btn = document.querySelector("button");
+btn.textContent = siteContent['cta']['button'];
+let h4s = document.querySelectorAll('h4');
+h4s[0].textContent = siteContent['main-content']['features-h4'];
+h4s[1].textContent = siteContent['main-content']['about-h4'];
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+h4s[2].textContent = siteContent['main-content']['services-h4'];
+h4s[3].textContent = siteContent['main-content']['product-h4'];
+h4s[4].textContent = siteContent['main-content']['vision-h4'];
+let paragraphs = document.querySelectorAll('p');
+paragraphs[0].textContent = siteContent['main-content']['features-content'];
+paragraphs[1].textContent = siteContent['main-content']['about-content'];
+paragraphs[2].textContent = siteContent['main-content']['services-content'];
+paragraphs[3].textContent = siteContent['main-content']['product-content'];
+paragraphs[4].textContent = siteContent['main-content']['vision-content'];
+h4s[5].textContent = siteContent['contact']['contact-h4'];
+paragraphs[5].textContent = siteContent['contact']['address']
+paragraphs[6].textContent = siteContent['contact']['phone']
+paragraphs[7].textContent = siteContent['contact']['email']
+paragraphs[8].textContent = siteContent['footer']['copyright']
